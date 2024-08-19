c7bc710186d234a278e9e9bae4bc51dc6709eccc
Anthony Castillo <acast698@fiu.edu>
2024-08-19 14:55:04 -0400

    Updated layout.tsx for link preview

--- gablesorientation/src/app/layout.tsx
+++ gablesorientation/src/app/layout.tsx
@@ -7,6 +7,25 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Coral Gables Internship Orientation",
   description: "Welcome to the City of Coral Gables Innovation and Technology Department!",
+  openGraph: {
+    title: "Coral Gables Internship Orientation",
+    description: "Welcome to the City of Coral Gables Innovation and Technology Department!",
+    images: [
+      {
+        url: "https://miamifilmfestival.com/city-of-coral-gables-logo/",
+        width: 800,
+        height: 600,
+        alt: "City of Coral Gables Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Coral Gables Internship Orientation",
+    description: "Welcome to the City of Coral Gables Innovation and Technology Department!",
+    images: ["https://miamifilmfestival.com/city-of-coral-gables-logo/"],
+  },
 };
 
 export default function RootLayout({
